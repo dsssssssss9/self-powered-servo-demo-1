64365e6eb0ef71db17222db9a3bbcd380ac5a10c
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-03-12 14:57:40 +0000

    Restore '12/03/2024, 13:31:50 Add jacdac extension + 2 buttons'

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
+    basic.showString("1")
+    ContinuousServo.spin_one_way(AnalogPin.P2)
+})
 input.onButtonPressed(Button.A, function () {
     ContinuousServo.spin_one_way(AnalogPin.P2)
     basic.showString("A")
@@ -10,7 +14,11 @@ input.onButtonPressed(Button.B, function () {
     ContinuousServo.spin_other_way(AnalogPin.P2)
     basic.showString("B")
 })
-basic.showIcon(IconNames.Heart)
+modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
+    basic.showString("2")
+    ContinuousServo.spin_other_way(AnalogPin.P2)
+})
+basic.showIcon(IconNames.Square)
 ContinuousServo.turn_off_motor(DigitalPin.P2)
 basic.forever(function () {
 	
