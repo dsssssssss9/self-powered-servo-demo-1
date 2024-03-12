b6ad716d7a5b1c1dee4408326cbcb48a458e4825
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-03-12 14:44:15 +0000

    Restore '12/03/2024, 13:24:53 Initial files for MakeCode project'

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
-modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
-    basic.showString("1")
-    ContinuousServo.spin_one_way_with_speed(AnalogPin.P2, modules.potentiometer1.position())
+input.onButtonPressed(Button.A, function () {
+    ContinuousServo.spin_one_way(AnalogPin.P2)
+    basic.showString("A")
 })
 input.onButtonPressed(Button.AB, function () {
     ContinuousServo.turn_off_motor(DigitalPin.P2)
     basic.showString("S")
 })
-modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
-    basic.showString("2")
-    ContinuousServo.spin_other_way_with_speed(AnalogPin.P2, modules.potentiometer1.position())
+input.onButtonPressed(Button.B, function () {
+    ContinuousServo.spin_other_way(AnalogPin.P2)
+    basic.showString("B")
 })
-basic.showIcon(IconNames.Angry)
+basic.showIcon(IconNames.Heart)
 ContinuousServo.turn_off_motor(DigitalPin.P2)
+basic.forever(function () {
+	
+})
