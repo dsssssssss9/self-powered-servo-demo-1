870b9269b990b3c98f862ed94e971e034e652164
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-03-12 14:08:39 +0000

    use speed control blocks

--- main.ts
+++ main.ts
@@ -1,25 +1,14 @@
 modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
     basic.showString("1")
-    ContinuousServo.spin_one_way(AnalogPin.P2)
-})
-input.onButtonPressed(Button.A, function () {
-    ContinuousServo.spin_one_way(AnalogPin.P2)
-    basic.showString("A")
+    ContinuousServo.spin_one_way_with_speed(AnalogPin.P2, 25)
 })
 input.onButtonPressed(Button.AB, function () {
     ContinuousServo.turn_off_motor(DigitalPin.P2)
     basic.showString("S")
 })
-input.onButtonPressed(Button.B, function () {
-    ContinuousServo.spin_other_way(AnalogPin.P2)
-    basic.showString("B")
-})
 modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
     basic.showString("2")
-    ContinuousServo.spin_other_way(AnalogPin.P2)
+    ContinuousServo.spin_other_way_with_speed(AnalogPin.P2, 99)
 })
 basic.showIcon(IconNames.Square)
 ContinuousServo.turn_off_motor(DigitalPin.P2)
-basic.forever(function () {
-	
-})
